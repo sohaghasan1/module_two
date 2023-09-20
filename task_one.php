42df5fa252ceb0fcfb42211ded9f69c761f51543
sohaghasan1 <33021476+sohaghasan1@users.noreply.github.com>
2023-09-20 17:28:41 +0600

    task_one.php

--- task_one.php
+++ task_one.php
@@ -5,12 +5,19 @@ echo " <h3>Looping with Increment using a Function and for loop<h3> ";
 
 
 function loopingWithFor($start_as, $end_as, $step){
-    for($i = $start_as; $i <= $end_as; $i = $i + $step){
-        echo $i . "\n";
+    if($start_as % 2 == 0){
+        for($i = $start_as; $i <= $end_as; $i = $i + $step){
+            echo $i . "\n";
+        }
+    }else{
+        $start_as += 1;
+        for($i = $start_as; $i <= $end_as; $i = $i + $step){
+            echo $i . "\n";
+        }
     }
 }
 
-loopingWithFor(2, 20, 2) . "<br>";
+loopingWithFor(1, 20, 2) . "<br>";
 // function(initial, end, step);
 
 
@@ -19,14 +26,25 @@ echo "<h3>Looping with Increment using a Function and while loop </h3>";
 
 
 function loopingWithWhile($start_as, $end_as, $step){
-    $i = $start_as;
-    while($i <= $end_as){
-        echo $i . "\n";
-        $i += $step;
+
+    if($start_as % 2 == 0){
+        $i = $start_as;
+        while($i <= $end_as){
+            echo $i . "\n";
+            $i += $step;
+        }
+    }else{
+        $start_as += 1;
+        $i = $start_as;
+        while($i <= $end_as){
+            echo $i . "\n";
+            $i += $step;
+        }
     }
+  
 }
 
-loopingWithWhile(2, 20, 2) . "<br>";
+loopingWithWhile(1, 20, 2) . "<br>";
 // function(initial, end, step);
 
 
@@ -37,12 +55,24 @@ echo "<h3>Looping with Increment using a Function and do while loop<h3>";
 
 
 function loopingWithDoWhile($start_as, $end_as, $step){
-        $i = $start_as;
-        do{
-           echo $i . "\n";
-           $i = $i + $step;
-        }while($i <= $end_as);
+
+        if($start_as % 2 == 0){
+            $i = $start_as;
+            do{
+               echo $i . "\n";
+               $i = $i + $step;
+            }while($i <= $end_as);
+        }else{
+            $start_as += 1;
+            $i = $start_as;
+            do{
+               echo $i . "\n";
+               $i = $i + $step;
+            }while($i <= $end_as);
+        }
+
+     
 }
 
-loopingWithDoWhile(2, 20, 2) . "<br>";
+loopingWithDoWhile(1, 20, 2) . "<br>";
 // function(initial, end, step);
